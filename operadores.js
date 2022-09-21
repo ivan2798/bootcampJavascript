b8527cfc07aba04b6b3107f524a0026228372fe0
ivan2798 <inge1998@hotmail.com>
2022-09-21 12:43:37 -0500

    operadores

---
+++ operadores.js
@@ -0,0 +1,36 @@
+//matematicos
+/*
++
+-
+*
+/
+*/
+
+let a = 10;
+let b =10
+
+//incrementar
+a = a+1;
+a++;
+
+//decrementar
+b = b-2;
+b--;
+b--;
+
+b-=5; // quita 5 unidades a b
+let resultado = a +b;
+
+resultado = a - b;
+//alert(resultado);
+resultado = a * b;
+console.log(resultado);
+
+
+resultado = a / b;
+console.log(resultado);
+resultado = a% b;
+console.log(resultado);
+
+
+
---
+++ suma.js
@@ -0,0 +1,24 @@
+/**
+ * Escribir un programa que sume dos numeros
+ */
+//defino las tres variables
+let valor1;
+let valor2
+let resultado;
+
+//ahora leer el valor1
+valor1=10;
+//ahora leer el valor2
+valor2=30;
+
+//operacion
+resultado = valor1 + valor2;
+
+//mostrar resultado
+console.log(resultado);
+
+
+let edad = 10;
+let apellido = "Herrera";
+
+//fin
