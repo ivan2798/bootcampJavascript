9220932c831b99e384b71843a7faeed83cdaf68c
ivan2798 <inge1998@hotmail.com>
2022-09-21 13:03:38 -0500

    relacionales

--- operadores.js
+++ operadores.js
@@ -33,4 +33,24 @@ resultado = a% b;
 console.log(resultado);
 
 
+//relacionales 
+/*
+<
+>
+<=
+>=
+*/
+
+let edadCarlos = 29;
+let edadNicolasManuel = 30;
+let esCarlosMayor = edadCarlos < edadNicolasManuel; //true
+console.log(esCarlosMayor);
+
+//distintos
+console.log(edadCarlos != edadNicolasManuel);
+
+let soyCarlos = true;
+let noSoyCarlos = !soyCarlos;
+
+
 
